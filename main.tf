c5a55fa17f0f0c416dad2cd6c975d27f06294e35
arun-kumar34 <arun.kumar@bootlabstech.com>
2022-03-28 08:30:30 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,68 @@
+//Add more resource blocks for other apis based on need.
+//Follow the same list construct
+
+//PROJECT
+resource "google_project_iam_binding" "binding" {
+  for_each = { for binding in var.google_project_iam_bindings : binding.unique => binding }
+  project  = each.value.project
+  role     = each.value.role
+  members  = each.value.members
+
+  dynamic "condition" {
+    for_each = each.value.condition != null ? [1] : [0]
+    content {
+			title			  = condition.value.title
+			description = condition.value.description
+			expression  = condition.value.expression
+		}
+  }
+}
+
+resource "google_project_iam_member" "member" {
+  for_each = { for member in var.google_project_iam_members : member.unique => member }
+  project  = each.value.project
+  role     = each.value.role
+  member   = each.value.member
+
+  dynamic "condition" {
+    for_each = each.value.condition != null ? [1] : [0]
+    content {
+			title			  = condition.value.title
+			description = condition.value.description
+			expression  = condition.value.expression
+		}
+  }
+}
+
+//STORAGE_BUCKET
+resource "google_storage_bucket_iam_binding" "binding" {
+  for_each = { for binding in var.google_storage_bucket_iam_bindings : binding.unique => binding }
+  bucket   = each.value.bucket
+  role     = each.value.role
+  members  = each.value.members
+
+  dynamic "condition" {
+    for_each = each.value.condition != null ? [1] : [0]
+    content {
+			title			  = condition.value.title
+			description = condition.value.description
+			expression  = condition.value.expression
+		}
+  }
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  for_each = { for member in var.google_storage_bucket_iam_members : member.unique => member }
+  bucket   = each.value.bucket
+  role     = each.value.role
+  member   = each.value.member
+
+  dynamic "condition" {
+    for_each = each.value.condition != null ? [1] : [0]
+    content {
+			title			  = condition.value.title
+			description = condition.value.description
+			expression  = condition.value.expression
+		}
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,65 @@
+//PROJECT
+variable "google_project_iam_bindings" {
+  type = list(object({
+    unique               = number,
+    project              = string,
+    role                 = string,
+    members              = list(string),
+    condition            = optional(object({
+        title       = string,
+        expression  = string,
+        description = optional(string)
+    }))
+  }))
+  description = "List of project iam members."
+  default     = []
+}
+
+variable "google_project_iam_members" {
+  type = list(object({
+    unique               = number,
+    project              = string,
+    role                 = string,
+    member               = string,
+    condition            = optional(object({
+        title       = string,
+        expression  = string,
+        description = optional(string)
+    }))
+  }))
+  description = "List of project iam members."
+  default     = []
+}
+
+//STORAGE
+variable "google_storage_bucket_iam_bindings" {
+  type = list(object({
+    unique               = number,
+    bucket               = string,
+    role                 = string,
+    members              = list(string),
+    condition            = optional(object({
+        title       = string,
+        expression  = string,
+        description = optional(string)
+    }))
+  }))
+  description = "List of storage bucket iam bindings."
+  default     = []
+}
+
+variable "google_storage_bucket_iam_members" {
+  type = list(object({
+    unique               = number,
+    bucket               = string,
+    role                 = string,
+    member               = string,
+    condition            = optional(object({
+        title       = string,
+        expression  = string,
+        description = optional(string)
+    }))
+  }))
+  description = "List of storage bucket iam members."
+  default     = []
+}
